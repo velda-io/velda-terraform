9612f66903d40bdb030c86f489430f70dfb6c954
Chuan Qiu <chuanq@velda.io>
2025-08-29 12:10:20 -0700

    Default instance profile to null

--- aws/controller/output.tf
+++ aws/controller/output.tf
@@ -8,7 +8,7 @@ output "agent_configs" {
     subnet_ids         = var.subnet_ids
     security_group_ids = [aws_security_group.agent_sg.id]
     controller_ip      = aws_instance.controller.private_ip
-    instance_profile   = aws_iam_instance_profile.agent_profile.name
+    instance_profile   = null
     use_nat            = var.use_nat
   }
 }
